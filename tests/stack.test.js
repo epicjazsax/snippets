97f54eb2b0ed388ef68fcf605b0352df63c50d65
Justin <121533926+epicjazsax@users.noreply.github.com>
2025-03-17 16:14:19 -0700

    Further refactor original tests

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -4,25 +4,35 @@ describe('Stack', () => {
 	const stack = new Stack;
 
 	test('contains a method to show its items', () => {
-		expect(stack.show()).toBeDefined()
+		const showSpy =  jest.spyOn(stack, 'show');
+		const shownList = stack.show();
+		const internalList = stack.items;
+
+		expect(showSpy).toHaveBeenCalled();
+		expect(shownList).toEqual(internalList);
+
+		showSpy.mockClear();
 	})
 	
 	test('pushes an item to the stack', () => {
 		const pushSpy = jest.spyOn(stack, 'push');
-		stack.push('foo');
+		const pushedValue = 'Push Foo';
+		stack.push(pushedValue);
 
-		expect(pushSpy).toHaveBeenCalledWith('foo');
+		expect(pushSpy).toHaveBeenCalledWith(pushedValue);
+		expect(stack.items).toContain(pushedValue);
 
 		pushSpy.mockClear();
 	})
 
 	test('pops one item from the stack', () => {
 		const popSpy = jest.spyOn(stack, 'pop');
-		stack.push('bar');
+		const initialValue = 'Pop Bar';
+		stack.push(initialValue);
 		const poppedValue = stack.pop();
 
-		expect(poppedValue).toEqual('bar');
 		expect(popSpy).toHaveBeenCalled();
+		expect(poppedValue).toEqual(initialValue);
 
 		popSpy.mockClear();
 	})
