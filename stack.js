b50427477486acc22e0f35869f84aff4271a3c3a
Justin <121533926+epicjazsax@users.noreply.github.com>
2025-03-18 14:21:12 -0700

    Make items property private

--- stack.js
+++ stack.js
@@ -1,27 +1,25 @@
 class Stack {
-	constructor() {
-		this.items = []
-	}
+	#items = []
 
 	push(values) {
 		if (values) {
 			if (typeof values == 'object') {
 				values.forEach((value) => {
-					this.items.push(value)
+					this.#items.push(value)
 				})
 				return
 			}
-			return this.items.push(values)
+			return this.#items.push(values)
 		}
 		throw new Error('must give a value to push')
 	}
 
 	pop() {
-		return this.items.pop()
+		return this.#items.pop()
 	}
 
 	show() {
-		return this.items
+		return this.#items
 	}
 };
 
