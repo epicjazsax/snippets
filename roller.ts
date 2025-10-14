1319dc25374a04602d37c134fd4d7b249d889ebe
Justin Zakrajsek <justin.zakrajsek@gmail.com>
2025-10-14 14:56:36 -0700

    Begin new dice-rolling snippet

---
+++ roller.ts
@@ -0,0 +1,7 @@
+// FUNCTION TO ROLL DICE
+//
+// future features include:
+// -roll any die
+// -roll multiple dice
+// -roll dice of multiple varieties
+// -special readout for certain rolls (ex. nat1/nat20, snakeeyes)??
