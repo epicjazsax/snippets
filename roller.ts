61fc6dea31054c593365a0178fe7d64ffc53a24f
Justin Zakrajsek <justin.zakrajsek@gmail.com>
2025-11-12 13:10:58 -0800

    Separate out rolling math and delete junk

--- roller.ts
+++ roller.ts
@@ -42,10 +42,7 @@ export class Roller {
 
     roll(die: any) {
         if ((typeof (die)) === 'number') {
-            if (!Number.isInteger(die)) {
-                throw new Error('number must be an integer!')
-            }
-            return (Math.floor((Math.random() * (die)) + 1))
+            return this.rollSingle(die)
         }
 
         if ((typeof (die)) === 'string') {
@@ -53,50 +50,18 @@ export class Roller {
 
             let arrayOfRolledDice: number[] = [];
             for (let n = 0; n < quantity; n++) {
-                arrayOfRolledDice.push(Math.floor((Math.random() * sides) + 1));
+                arrayOfRolledDice.push(this.rollSingle(sides));
             }
             return arrayOfRolledDice
         }
 
         throw new Error('argument type not supported')
     }
+    rollSingle(sidesOnDie: number): number {
+        if (!Number.isInteger(sidesOnDie)) {
+            throw new Error('number of sides on die must be an integer!')
+        }
+        return Math.floor((Math.random() * sidesOnDie) + 1)
+    }
 };
-
-// export class DiceStringValidator {
-//     validate(dice: string) {
-//         let validity: boolean = false;
-
-//         if (!dice.includes('d')) {
-//             throw new Error('missing dice delineator "d"')
-//         }
-
-//         const quantityAndSidesOfDice: string[] = dice.split('d');
-
-//         if (quantityAndSidesOfDice.length !== 2) {
-//             throw new Error('must give only one delineator "d"')
-//         }
-//         if (!quantityAndSidesOfDice[0]) {
-//             throw new Error('must give quantity of dice to roll')
-//         }
-//         if (!quantityAndSidesOfDice[1]) {
-//             throw new Error('must give number of sides on dice to roll')
-//         }
-//         if (isNaN(+quantityAndSidesOfDice[0])) {
-//             throw new Error('quantity must be a number')
-//         }
-//         if (isNaN(+quantityAndSidesOfDice[1])) {
-//             throw new Error('sides must be a number')
-//         }
-//         if (Math.floor(+quantityAndSidesOfDice[0]) !== +quantityAndSidesOfDice[0]) {
-//             throw new Error('quantity must be an integer')
-//         }
-//         if (Math.floor(+quantityAndSidesOfDice[1]) !== +quantityAndSidesOfDice[1]) {
-//             throw new Error('sides must be an integer')
-//         }
-
-//         validity = true;
-
-//         return validity
-//     }
-// };
 
